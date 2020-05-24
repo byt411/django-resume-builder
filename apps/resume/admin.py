d9b3aff753d263318b817e3930c7d681128705bd
Brian <brian.yimtam@student.manchester.ac.uk>
2020-05-24 23:49:14 +0100

    Made model changes visible in admin panel.

--- apps/resume/admin.py
+++ apps/resume/admin.py
@@ -7,3 +7,9 @@ from . import models
 class ResumeItemAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'company', 'start_date')
     ordering = ('user', '-start_date')
+
+
+@admin.register(models.Resume)
+class ResumeItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name',)
+    ordering = ('user', 'name')
